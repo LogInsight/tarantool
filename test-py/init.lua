a8162e7c45ef49f93edf278c8a3108fcdd47b3c3
nzinfo <limn@loginsight.cn>
2015-11-20 22:10:33 +0800

    rename to wt_engine

--- test-py/init.lua
+++ test-py/init.lua
@@ -1,6 +1,9 @@
 box.cfg{listen=33013}
-box.schema.user.grant('guest', 'read,write,execute', 'universe')
-s = box.schema.space.create('tmp', {temporary=true})
+s = box.schema.space.create('tmp', {temporary=false, engine='wiredtiger'})
 s:create_index('primary',{parts = {1,'STR', 2, 'STR'}})
+-- box.schema.user.grant('guest', 'read,write,execute', 'universe')
 
+s:insert{'Hello', 'world'}
+
+s:drop()
 
